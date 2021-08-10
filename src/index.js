d69f6996a67fd5878c4e822a475622f192855def
liminalfun <xelanawotka@gmail.com>
2021-08-10 19:28:51 -0400

    [FIX] move telescopic.js higher in list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.css";
+import "./js/telescopic.js";
 import "./js/nav.js";
 import "./js/imgdelivery.js";
 import "./js/cursors.js";
-import "./js/telescopic.js";
